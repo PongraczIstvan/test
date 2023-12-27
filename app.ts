79d7f8d77a60a32d6505e604195bb00cb65ce525
Julio <pongracz13@qualitax.hu>
2023-12-27 13:56:38 +0100

    sum error

--- app.ts
+++ app.ts
@@ -1,41 +1,107 @@
 //import fb from 'node-firebird';
+import { FirebirdPool } from 'ts-firebird';
 import Config from './fb_config';
-import { pool } from 'node-firebird';
-function queryDatabase(pool: any): Promise<any> {
+import { pool, Database, ConnectionPool } from 'node-firebird';
+
+function getDatabase(pool:ConnectionPool): Promise<Database> {
   return new Promise((resolve, reject) => {
-    pool.get((err: any, db: any) => {
+    pool.get((err: any, db: Database) => {
       if (err) {
         reject(err);
         return;
       }
-
-      db.query('SELECT * FROM AFA_KODOK order by 1 DESC', [], (err: any, result: any) => {
+      resolve(db);
+    });
+  });
+}
+function queryDatabase(db: Database): Promise<any> {
+  return new Promise((resolve, reject) => {
+   
+      db.query('select fok_tetel.fok1 FOK, cast(sum(fok_tetel.osszeg) as numeric(15,2)) osszeg from fok_tetel group by fok_tetel.fok1',
+       [], (err: any, result: any) => {
         if (err) {
           db.detach();
           reject(err);
           return;
-        }
-
-        db.detach();
+        }        
         resolve(result);
       });
     });
-  });
-}
+  }
 
-async function test() {
+
+async function test2() {
   console.log(`Start ${new Date().toISOString()}`);  
+  console.log(Config);
   const pl = pool(5, Config);
 
   try {
-    const result = await queryDatabase(pl);
+    const db = await getDatabase(pl);
+    console.log('Connected to database');    
+    const result = await queryDatabase(db);
+    console.log('Query executed');
     console.log(result);
-    pl.destroy();
+    db.detach();    
   } catch (err) {
     console.error('HIBA !!!');
     console.error(err);
-    pl.destroy();
   }
+  pl.destroy();
 }
 
-test();
+async function test() {
+  const pool = new FirebirdPool(5, Config);
+
+  try{
+  const tr = await pool.getTransaction();
+/*
+  const sql=`select fok_tetel.fok1 FOK, sum(fok_tetel.osszeg) osszeg
+  from fok_tetel     
+   group by fok_tetel.fok1
+  `;
+*/
+  const sql=`select fok_tetel.fok1 FOK, fok_tukor.fok_neve, cast(sum(iif(fok_tetel.jelleg=0, fok_tetel.osszeg,0)) as numeric(15,2)) tartozik_osszeg, cast(sum(iif(fok_tetel.jelleg=1, fok_tetel.osszeg,0)) as numeric(15,2))
+kovetel_osszeg,cast(sum(iif(fok_tetel.jelleg=0, fok_tetel.osszeg,-fok_tetel.osszeg)) as numeric(15,2)) egyenleg
+from fok_tetel
+   left outer join fok_tukor on (fok_tetel.kapcs_ev = fok_tukor.kapcs_ev) and (fok_tetel.fok1 = fok_tukor.fok)
+
+where
+   (
+      (fok_tetel.kapcs_ev = ?)
+   and (fok_tetel.datum between ? and ?)
+   )
+group by fok_tetel.fok1 , fok_tukor.fok_neve
+order by fok_tetel.fok1
+ROWS ? to ?`;
+  // console.log(new Date('2022-01-01T00:00:00.000Z'));
+    const param: any[] = ['2022',new Date('2022-01-01T00:00:00.000Z'), new Date('2022-01-31T00:00:00.000Z'), 1, 500];
+
+    const result = await tr.query(sql, param,true);
+    console.log(result);
+  }
+  catch(e){
+    
+    if (e instanceof Error) {
+      console.error('Ez egy Error típusú hiba:', e.name, e.message);      
+      if ('gdscode' in e) {
+        console.error("gdscode:", e.gdscode);       // 335544569
+      }
+    } else if (e instanceof TypeError) {
+      console.error('Ez egy TypeError típusú hiba:', e);
+    } else {
+      console.error('Ismeretlen hiba típus:', e);
+    }
+  } 
+  //tr.commit();
+  pool.destroy();
+  
+}
+async function main() {
+console.log('Start');
+//await test2();
+await test();
+
+}
+
+main();
+
--- fb_config.ts
+++ fb_config.ts
@@ -1,8 +1,8 @@
 import { Options } from 'node-firebird';
 export default <Options> {
-    host : '192.168.177.142',
+    host : '192.168.177.155',
     port : 3060,
     user : 'SYSDBA',
     password : 'masterkey',
-    database : 'C:/wintax3/c320.FDB'
+    database : 'C:/wintax3/ceg_c300.FDB'
 }
